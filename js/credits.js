9e993c689ac47411d510fb8fbc47badb9ee187f5
Adangc <88640078+adanglol@users.noreply.github.com>
2022-11-17 12:03:01 -0800

    Added Credits
    Addded credits to our page and looks like our API is working

---
+++ js/credits.js
@@ -0,0 +1,12 @@
+// This is a blank JS file
+//If you made it this far that means you clicked the link
+
+//Here credit goes out to
+//Wei-Yan Chiang
+//Emma Lapolt
+//Angela Rangel
+//Aaron Dangc
+
+//Thank you for visiting our page the goal was to raise awareness to endangered species because it is no joke
+
+//We thought making a web page and incoporating an API can help us convey that to the user.
