d851e7e61e22c7dc58beec300947b3960b110bd3
Adangc <88640078+adanglol@users.noreply.github.com>
2022-11-17 00:11:50 -0800

    Final Comiit
    working on endangered park descriptions are there but 503 request meaning server is overloadded or under maintance

--- js/endangerpark.js
+++ js/endangerpark.js
@@ -23,16 +23,7 @@ ecosApi.done(function(data){
     $(".con.here").html("<h1>"+ data["data"][5][0]+"</h1>" + "<h2>"+ "Scientific Name : " +
     data["data"][5][1]["value"]+"</h2>" + "<h2>"+"Status : " + data["data"][5][2] +"</h2>" +
     "<h2>"+"Reference Link : "+ data["data"][5][1]["url"]+ "</h2>" + "<br />" +
-    "<p>Papasula abbotti, or Abbott’s booby, is an endangered seabird of the sulid family, " +
-    "which includes gannets and boobies. They breed only in a few spots on the Australian " +
-    "territory of Christmas Island in the eastern Indian Ocean, although it formerly had " +
-    "a much wider range.</p>" + "<p>The bird has white plumage with black markings, and is adapted "+
-    "for long-distnace flight. It forages around Christmas Island, often around nutrient-rich oceanic " +
-    "upwellings, although individuals can travel for thousands of kilometers. This bird also has a unique " +
-    "blue-colored webbed feet. Males have pale grey bills with a black tip, whereas females have pink bills with "+
-    "a black tip. </p>" + "<p>There are various cause of decreasing population, including but not limited to its " +
-    "former habitats logged to make way for phosphate mining; introduction of yellow crazy ants, which decreased "+
-    "habitat quality. All nesting areas are now included in a national park. </p>");
+    "<p>Papasula abbotti, or Abbott’s booby, is an endangered seabird that breed only in a few spots on the Australian territory of Christmas Island in the eastern Indian Ocean, although it formerly had a much wider range. The bird forages around Christmas Island, often around nutrient-rich oceanic upwellings. This bird has unique blue-colored webbed feet. Males have pale grey bills with a black tip, whereas females have pink bills with a black tip. There are various cause of decreasing population, including but not limited to its former habitats logged to make way for phosphate mining; introduction of yellow crazy ants, which decreased habitat quality. </p>");
     //Source: https://en.wikipedia.org/wiki/Abbott%27s_booby
   });
 
@@ -50,8 +41,12 @@ ecosApi.done(function(data){
     $(".con2").toggleClass("here");
     $(".con2.here").html("<h1>"+ data["data"][38][0]+"</h1>" + "<h2>"+ "Scientific Name : " +
     data["data"][38][1]["value"]+"</h2>" + "<h2>"+"Status : " + data["data"][38][2] +"</h2>" +
-    "<h2>"+"Reference Link : "+ data["data"][38][1]["url"]+ "</h2>" + "<br />" +
-    "<p>This smallest of the crocodile species, at only 5 feet long, inhabits rainforest and water habitats of West Africa. The adults of this species can be noted by their dark back.</p>");
+    "<h2>"+"Reference Link : "+ data["data"][38][1]["url"]+ "</h2>" + "<br />" + "<p>The smallest of the crocodile species, at only 5 feet long, inhabits the tropics of West and Central"+
+    " Africa in mostly rainforests and mid-depth bodies of water. The adults of this species can be noted by their "+
+    "dark backside and yellow underside with black patches. The young, however, have lighter brown stripes across their "+
+    "backs and tails, with yellow marks on their heads. Dwarf crocodiles eat various bugs and meat. Currently, there are "+
+    "between 25,000 and 100,000 African dwarf crocodiles in the world. Data shows that their population is put at risk "+
+    "mainly because they are hunted for their meat and leather, and due to a loss of habitat from deforestation.</p>");
 
 
   });
@@ -62,7 +57,13 @@ ecosApi.done(function(data){
     $(".con3.here").html("<h1>"+ data["data"][40][0]+"</h1>" + "<h2>"+ "Scientific Name : " +
     data["data"][40][1]["value"]+"</h2>" + "<h2>"+"Status : " + data["data"][40][2] +"</h2>" +
     "<h2>"+"Reference Link : "+ data["data"][40][1]["url"]+ "</h2>" + "<br />" +
-    "<p>TEXT HERE DESCRIBE CREATURE</p>");
+    "<p>The African penguin is a small to medium species of penguin. This species' distinctive feature is the pink around their "+
+    "eyes, as well as their stubby beak and black coloring around their face. These penguins are found only on the south-west "+
+    "coast of Africa. There are two different colonies of African penguins on the mainland in the tourist town Boulders Beach. "+
+    "As coastal creatures, African penguins eat pelagic fish. Currently, there are 41,700 African penguins in the world. "+
+    "As recently as the 1970s African penguin eggs were considered a delicacy in cuisine. Eggs were smashed all along the "+
+    "coast to ensure that the thousands of eggs collected days later were all fresh. This practice going on for decades, in "+
+    "addition to oil spills dating back to the 1930s, took a dramatic toll on the African penguins' population.</p>");
 
   });
 
@@ -72,7 +73,7 @@ ecosApi.done(function(data){
     $(".con4.here").html("<h1>"+ data["data"][42][0]+"</h1>" + "<h2>"+ "Scientific Name : " +
     data["data"][42][1]["value"]+"</h2>" + "<h2>"+"Status : " + data["data"][42][2] +"</h2>" +
     "<h2>"+"Reference Link : "+ data["data"][42][1]["url"]+ "</h2>" + "<br />" +
-    "<p>TEXT HERE DESCRIBE CREATURE</p>");
+    "<p>The african vivviparous toad can be found in Cote d'lvoire, Guinea, and Libera. These frogs are the only toad who don't lay their eggs. They are a critically endangered species because of habitat loss dude to human expanding to their homes. Their diet consists of various insects: worms, ants, crickets, etc.</p>");
 
   });
 
@@ -87,13 +88,15 @@ ecosApi.done(function(data){
 
   });
 
+//credit : https://www.encyclopedia.com/environment/science-magazines/alabama-cave-shrimp
+//https://en.wikipedia.org/wiki/Alabama_cave_shrimp
   $("#shrimp").click(function(){
     $("#alabama").toggleClass("hide");
     $(".con6").toggleClass("here");
     $(".con6.here").html("<h1>"+ data["data"][91][0]+"</h1>" + "<h2>"+ "Scientific Name : " +
     data["data"][91][1]["value"]+"</h2>" + "<h2>"+"Status : " + data["data"][91][2] +"</h2>" +
     "<h2>"+"Reference Link : "+ data["data"][91][1]["url"]+ "</h2>" + "<br />" +
-    "<p>TEXT HERE DESCRIBE CREATURE</p>");
+    "<p>Palaemonias alabamae, or Alabama cave shrimps, is a species of shrimp found only in caves in the state of Alabama. They are colorless and largely transparent with a total length of up to 20mm, or 0.8 inches. Their diet consists of detritus and plant matter. The cave systems the shrimps are found in are threatened by pollution from urbanization, as groundwater contamination may result from seage leakage, industrial contaminants, road and highway runoff, and more. </p>");
   });
 
   //Append more endangered species name to our div to display more information on endangered species
